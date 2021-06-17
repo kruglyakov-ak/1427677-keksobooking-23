9042773fd29bbeaf1aa55bb17e6a62104fbefdeb
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-06-17 03:21:59 +0300

    Добавил скрытие блока если отсутствуют данные

--- js/generator-elems.js
+++ js/generator-elems.js
@@ -13,7 +13,9 @@ function generateCard(data) {
 
   // Цена
   card.querySelector('.popup__text--price').innerHTML = `<p>${data.offer.price}<span>₽/ночь</span></p>`;
-
+  if (typeof data.offer.price !== 'number') {
+    card.querySelector('.popup__text--price').remove();
+  }
   // Тип
   if (data.offer.type === 'palace') {
     card.querySelector('.popup__type').textContent = 'Дворец';
@@ -30,10 +32,16 @@ function generateCard(data) {
   // Количество комнат и гостей
   card.querySelector('.popup__text--capacity').innerHTML =
     `<p>${data.offer.rooms} комнаты для ${data.offer.guests} гостей</p>`;
+  if (typeof data.offer.guests !== 'number' || typeof data.offer.rooms !== 'number') {
+    card.querySelector('.popup__text--capacity').remove();
+  }
 
   // Время выезда и заезда
   card.querySelector('.popup__text--time').innerHTML =
     `<p>Заезд после ${data.offer.checkin}, выезд до ${data.offer.checkout}</p>`;
+  if (typeof data.offer.checkin !== 'number' || typeof data.offer.checkout !== 'number') {
+    card.querySelector('.popup__text--time').remove();
+  }
 
   // Особенности
   const featuresList = card.querySelector('.popup__features');
@@ -52,6 +60,9 @@ function generateCard(data) {
 
   // Фотографии
   const photos = card.querySelector('.popup__photos');
+  if (data.offer.photos.length <= 0) {
+    photos.remove();
+  }
   photos.querySelector('.popup__photo').setAttribute('src', data.offer.photos[0]);
   for (let i = 1; data.offer.photos.length > photos.children.length; i++) {
     const newPhoto = photos.querySelector('.popup__photo').cloneNode(true);
@@ -61,6 +72,12 @@ function generateCard(data) {
 
   // Аватарка
   card.querySelector('.popup__avatar').setAttribute('src', data.author.avatar);
+  card.querySelector('.popup__avatar').onerror = function () {
+    card.querySelector('.popup__avatar').setAttribute('src', 'img/avatars/default.png');
+  };
+  if (card.querySelector('.popup__avatar').getAttribute('src') === 'undefined') {
+    card.querySelector('.popup__avatar').remove();
+  }
 
   mapCanvas.appendChild(card);
 }
