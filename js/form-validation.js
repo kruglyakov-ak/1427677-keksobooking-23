69011d3b6587745e9ff85109e106c5b9c71ebf63
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-06-25 03:03:46 +0300

    П

--- js/form-validation.js
+++ js/form-validation.js
@@ -2,29 +2,21 @@ import {
   addInputValidationIndicator
 } from './util.js';
 
-
 const titleInput = document.querySelector('#title');
 const priceInput = document.querySelector('#price');
+
 const roomNumberList = document.querySelector('#room_number');
 const capacityList = document.querySelector('#capacity');
+const errorText = '1 комната — «для 1 гостя»;\n2 комнаты — «для 2 гостей» или «для 1 гостя»;\n3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;\n100 комнат — «не для гостей».';
 
 const validateRoomNumber = () => {
   roomNumberList.addEventListener('change', (evt) => {
     if (+evt.target.value !== 100 && +capacityList.value === 0) {
-      roomNumberList.setCustomValidity(`1 комната — «для 1 гостя»;\n
-      2 комнаты — «для 2 гостей» или «для 1 гостя»;\n
-      3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;\n
-      100 комнат — «не для гостей».`);
+      roomNumberList.setCustomValidity(errorText);
     } else if (+evt.target.value === 100 && +capacityList.value !== 0) {
-      roomNumberList.setCustomValidity(`1 комната — «для 1 гостя»;\n
-      2 комнаты — «для 2 гостей» или «для 1 гостя»;\n
-      3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;\n
-      100 комнат — «не для гостей».`);
+      roomNumberList.setCustomValidity(errorText);
     } else if (+evt.target.value < +capacityList.value) {
-      roomNumberList.setCustomValidity(`1 комната — «для 1 гостя»;\n
-      2 комнаты — «для 2 гостей» или «для 1 гостя»;\n
-      3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;\n
-      100 комнат — «не для гостей».`);
+      roomNumberList.setCustomValidity(errorText);
     } else {
       roomNumberList.setCustomValidity('');
     }
@@ -34,20 +26,11 @@ const validateRoomNumber = () => {
 const validateCapacity = () => {
   capacityList.addEventListener('change', (evt) => {
     if (+evt.target.value !== 0 && +roomNumberList.value === 100) {
-      capacityList.setCustomValidity(`1 комната — «для 1 гостя»;\n
-      2 комнаты — «для 2 гостей» или «для 1 гостя»;\n
-      3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;\n
-      100 комнат — «не для гостей».`);
+      capacityList.setCustomValidity(errorText);
     } else if (+evt.target.value === 0 && +roomNumberList.value !== 100) {
-      capacityList.setCustomValidity(`1 комната — «для 1 гостя»;\n
-      2 комнаты — «для 2 гостей» или «для 1 гостя»;\n
-      3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;\n
-      100 комнат — «не для гостей».`);
+      capacityList.setCustomValidity(errorText);
     } else if (+evt.target.value > +roomNumberList.value) {
-      capacityList.setCustomValidity(`1 комната — «для 1 гостя»;\n
-      2 комнаты — «для 2 гостей» или «для 1 гостя»;\n
-      3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»;\n
-      100 комнат — «не для гостей».`);
+      capacityList.setCustomValidity(errorText);
     } else {
       capacityList.setCustomValidity('');
     }
@@ -57,7 +40,6 @@ const validateCapacity = () => {
 const inputValidate = () => {
   addInputValidationIndicator(titleInput);
   addInputValidationIndicator(priceInput);
-
   validateRoomNumber();
   validateCapacity();
 };
